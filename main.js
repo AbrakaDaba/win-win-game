b985b8f09500774d37a66537f772d76a2299217b
David <abrakadaba@gmail.com>
2021-11-10 14:01:58 +0100

    shaking fists

--- main.js
+++ main.js
@@ -6,15 +6,25 @@ let player1 = document.querySelector('.game-pic-1');
 let player2 = document.querySelector('.game-pic-2');
 let result1 = document.querySelector('.result-1');
 let result2 = document.querySelector('.result-2');
-btn.addEventListener("click", playTheGame);
+btn.addEventListener("click", shakingFists);
 
 function playTheGame(){
+    player1.classList.remove('fist');
+    player2.classList.remove('fist');
     let rand1= Math.ceil(Math.random() * 3);
     let rand2= Math.ceil(Math.random() * 3);
     console.log(rand1, rand2);
     player1.setAttribute("src",rand1 + ".svg");
     player2.setAttribute("src",rand2 + ".svg");
     gameResult(rand1, rand2);
+}
+
+function shakingFists(){
+    setTimeout(playTheGame, 3000);
+    player1.classList.add('fist')
+    player1.setAttribute('src', '3.svg');
+    player2.classList.add('fist');
+    player2.setAttribute('src', '3.svg');
 }
 
 function gameResult(param1, param2){
